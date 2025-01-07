3029f0176cabd9bfc179b780f9ab87e37e4c6df4
Joseph Mwanzia <makamwanzia@gmail.com>
2025-01-07 23:57:16 +0300

    fix: implement pagination and filtering for the to-do list

--- controller/todoController.js
+++ controller/todoController.js
@@ -2,6 +2,7 @@ const Todo = require('../model/todoModel')
 const bcrypt = require('bcrypt');
 
 exports.fetchData = async function (req, res) {
+    const term = req.query.term
     const userId =req.user.id;
     const page = parseInt(req.query.page, 10) || 1; // Current page (default: 1)
     const limit = parseInt(req.query.limit, 10) || 10; // Items per page (default: 10)
@@ -9,8 +10,21 @@ exports.fetchData = async function (req, res) {
     const total = parseInt(await Todo.getCount(), 10)
 
     try {
-      const data = await Todo.all(userId, limit, offset)
-      res.status(200).send({data, page, limit, total})
+        const data = await Todo.all(userId, limit, offset)
+
+        if (!term) {
+            res.status(200).send({data, page, limit, total}) // if no search term is provided return all the posts
+        } else {
+            const filterdTodos = data.filter((todo) => {
+                return todo.title.toLowerCase().includes(term.toLowerCase()) ||
+                todo.description.toLowerCase().includes(term.toLowerCase())
+            })
+    
+           filterdTodos.length === 0 ?
+            res.sendStatus(404) // if filterd posts is not present send not found status 
+            : res.status(200).send({filterdTodos, page, limit, total}) // send the filterd posts
+        }
+
     } catch (error) {
         console.error(`Error executing GET request, ${error.message}`);
         res.status(500).send(`Error executing GET request, ${error.message}\n`)
