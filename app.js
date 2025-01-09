8706341013ba33f1157c76b0624802bb300efa57
Joseph Mwanzia <makamwanzia@gmail.com>
2025-01-10 00:38:29 +0300

    fix: fix the router issue not responding

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const router = require('./routes/todoRoutes')
+const router = require('./routes/routes')
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT
