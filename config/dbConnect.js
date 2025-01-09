04c4f148622a99e87235580ed2e836e5ed9c42b2
Joseph Mwanzia <makamwanzia@gmail.com>
2025-01-10 00:18:04 +0300

    style: format config/dbConnect

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,11 +1,12 @@
 const { Pool } = require('pg');
 require('dotenv').config()
+const { DB_HOST, DB_USER,DB_PASSWORD,DB_DATABASE } = process.env
 
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    host: DB_HOST,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_DATABASE,
     max: 20,
     idleTimeoutMillis: 3000,
     connectionTimeoutMillis: 2000
